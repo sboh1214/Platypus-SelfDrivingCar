71eef3998389081d2b7b79f342d098c73f1e9101
Seungbin Oh <sboh1214@gmail.com>
2020-12-04 09:20:02 +0900

    sync
    Co-authored-by: 박성현 <shyun00124@o365.ice.go.kr>

--- drive.py
+++ drive.py
@@ -1,8 +1,11 @@
 from gpiozero import Motor
 
+########################################
+#get_line_gradient=get_direction 입니다#
+########################################
 def control():  # :3
     motor = Motor(right=핀번호, left=핀번호)
-    if get_line_gradient() < 90도 :
+    if get_direction() < 90도 :
         motor.left(speed = 0.3) #todo
-    elif get_line_gradient() > 90도 : 
+    elif get_direction() > 90도 : 
         motor.right(speed = 0.3) #todo
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import time
 import numpy as np
 import cv2
 
+XMAX = 480
+
 def read_video(video_path: str, stop_frame: int) -> None:
+    """
+    영상을 읽어서 사진으로 변환
+    """
     vidcap = cv2.VideoCapture(video_path)
     count = 0
 
@@ -21,32 +26,95 @@ def read_video(video_path: str, stop_frame: int) -> None:
         if count > stop_frame:
             break
 
-    vidcap.release() 
+    vidcap.release()
+
+    #ㅇㅅㅇ#
 
 
 def show_canny(image_path: str):
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    edge = cv2.Canny(img, 50, 200)
+    pre_img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    img = region_selection(pre_img)  # 변환 지역 설정 후 처리
+    edge = cv2.Canny(img, 50, 200)  # 변환된 edge의 색 : (255,255,255)
     cv2.imshow('Result Image', edge)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
+def region_selection(image):
+    """
+    변환 지역 설정
+    """
+    mask = np.zeros_like(image)
+
+    if len(image.shape) > 2:
+        channel_count = image.shape[2]
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+
+    rows, cols = image.shape[:2]
+    bottom_left = [cols * 0.1, rows * 0.95]
+    top_left = [cols * 0.4, rows * 0.6]
+    bottom_right = [cols * 0.9, rows * 0.95]
+    top_right = [cols * 0.6, rows * 0.6]
+    vertices = np.array(
+        [[bottom_left, top_left, top_right, bottom_right]], dtype=np.int32)
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+
+def get_line():  # todo
+    pass
+
+
+def average_slope_intercept(lines):
+    """
+    교점과 각도를 찾는다
+    """
+    left_lines = []  # (slope, intercept)
+    left_weights = []  # (length,)
+    right_lines = []  # (slope, intercept)
+    right_weights = []  # (length,)
+
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            if x1 == x2:
+                continue
+            slope = (y2 - y1) / (x2 - x1)
+            intercept = y1 - (slope * x1)
+            length = np.sqrt(((y2 - y1) ** 2) + ((x2 - x1) ** 2))
+            if slope < 0:
+                left_lines.append((slope, intercept))
+                left_weights.append((length))
+            else:
+                right_lines.append((slope, intercept))
+                right_weights.append((length))
+    left_lane = np.dot(left_weights,  left_lines) / \
+        np.sum(left_weights) if len(left_weights) > 0 else None
+    right_lane = np.dot(right_weights, right_lines) / \
+        np.sum(right_weights) if len(right_weights) > 0 else None
+    return left_lane, right_lane
+
+
 def get_line_gradient():
-    switch=0
-    tmp=0
-    #canny에서 선분의 a, b, r, theta 받아오기
-    for _ in range(0, 1) : #todo
+    switch = 0
+    theta2 = 0
+    get_line_info()
+    for _ in range(0, 1):  # todo
         if a == XMAX:
-            tmp=theta
-            if switch==0:
-                theta1=theta
-                switch=1
-    return (tmp+theta1)/2
-        
-    
+            theta2 = theta
+            if switch == 0:
+                theta1 = theta
+                switch = 1
+    return (theta1+theta2)/2
+
+
 def get_direction(theta1: float, theta2: float):
     theta = (theta1+theta2)/2
+    return theta
+
 
 if __name__ == "__main__":
-    read_video(f"./videos/20200925_091111.mp4", 1000)
-    show_canny(f"./images/frame_{500}.jpg")
+    read_video(f"./videos/20200925_091111.mp4", 200)
+    show_canny(f"./images/frame_{100}.jpg")
